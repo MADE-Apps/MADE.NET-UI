654a3f2556820e6d1f5347100b4f7a58104e0da4
James Croft <jamz_c@hotmail.co.uk>
2022-04-27 15:01:22 +0100

    Updated to support MADE.NET FluentValidation package

--- samples/MADE.Samples/MADE.Samples.Shared/Features/Samples/ViewModels/InputValidatorPageViewModel.cs
+++ samples/MADE.Samples/MADE.Samples.Shared/Features/Samples/ViewModels/InputValidatorPageViewModel.cs
@@ -1,7 +1,9 @@
 namespace MADE.Samples.Features.Samples.ViewModels
 {
     using System;
+    using System.Collections.Generic;
     using CommunityToolkit.Mvvm.Messaging;
+    using FluentValidation;
     using MADE.Data.Validation;
     using MADE.Data.Validation.Validators;
     using MADE.UI.Views.Navigation;
@@ -11,13 +13,15 @@ namespace MADE.Samples.Features.Samples.ViewModels
     {
         private string inputText;
         private DateTimeOffset? inputDate;
+        private string fluentValidationInputText;
 
         public InputValidatorPageViewModel(INavigationService navigationService, IMessenger messenger)
             : base(navigationService, messenger)
         {
         }
 
-        public ValidatorCollection InputTextValidators { get; } = new ValidatorCollection { new RequiredValidator(), new MaxLengthValidator(16) };
+        public ValidatorCollection InputTextValidators { get; } =
+            new ValidatorCollection { new RequiredValidator(), new MaxLengthValidator(16) };
 
         public string InputText
         {
@@ -25,12 +29,41 @@ namespace MADE.Samples.Features.Samples.ViewModels
             set => this.SetProperty(ref inputText, value);
         }
 
-        public ValidatorCollection InputDateValidators { get; } = new ValidatorCollection { new RequiredValidator(), new BetweenValidator(DateTimeOffset.Now.AddDays(-7), DateTimeOffset.Now.AddDays(7)) };
+        public ValidatorCollection InputDateValidators { get; } = new ValidatorCollection
+        {
+            new RequiredValidator(),
+            new BetweenValidator(DateTimeOffset.Now.AddDays(-7), DateTimeOffset.Now.AddDays(7))
+        };
 
         public DateTimeOffset? InputDate
         {
             get => inputDate;
             set => this.SetProperty(ref inputDate, value);
+        }
+
+        public FluentValidatorCollection<string> FluentValidationValidators { get; } = new(GetFluentValidators());
+
+        public string FluentValidationInputText
+        {
+            get => fluentValidationInputText;
+            set => this.SetProperty(ref fluentValidationInputText, value);
+        }
+
+        private static IEnumerable<AbstractValidator<string>> GetFluentValidators()
+        {
+            var requiredValidator = new InlineValidator<string>();
+            requiredValidator
+                .RuleFor(x => x)
+                .NotEmpty()
+                .WithMessage("A value is required.");
+
+            var maxLengthValidator = new InlineValidator<string>();
+            maxLengthValidator
+                .RuleFor(x => x.Length)
+                .LessThanOrEqualTo(16)
+                .WithMessage("The length of the value must be less than or equal to 16 characters.");
+
+            return new[] { requiredValidator, maxLengthValidator };
         }
     }
 }
